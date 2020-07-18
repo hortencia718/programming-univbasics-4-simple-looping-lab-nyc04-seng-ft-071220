f4f48a679dcae0e656b5041063766d11a048cde4
hortencia c <hortenciaecisneros@gmail.com>
2020-07-18 22:14:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -19,11 +19,12 @@ end
 
  def return_string_array(array)
   count = 0 
-  new_array =[]
-  while count < array.length do 
+  while count < array.length do
+    array[count] =
     array[count].to_s 
     count += 1 
   end 
+  array
 end 
   
-end
+
